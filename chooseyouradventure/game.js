0cbe8db445e4740418373eb2b9d66786dcbf848d
joshuajames <joshua.j399@stu.nebo.edu>
2019-01-23 10:27:09 -0700

    ask and stores name

---
+++ chooseyouradventure/game.js
@@ -0,0 +1,19 @@
+// single line comment
+/*
+
+multiline comment
+
+*/
+
+//document.write("<h1>I cant wait for <em>spring</em></h1>");
+
+
+//alert("warning! will robinson... warning");
+
+//confirm("Do you like Pokemon?");
+
+//prompt("What type of pokemon do you like?");
+
+var pc = prompt("What is thine name?");
+
+confirm("So your name is "+pc);
