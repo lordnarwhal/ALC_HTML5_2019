d43b6e9417c527751e255688d9129a9d7782c560
joshuajames <joshua.j399@stu.nebo.edu>
2019-01-25 10:26:45 -0700

    started game

--- chooseyouradventure/game.js
+++ chooseyouradventure/game.js
@@ -14,6 +14,34 @@ multiline comment
 
 //prompt("What type of pokemon do you like?");
 
-var pc = prompt("What is thine name?");
+//var pc = prompt("What is thine name?");
 
-confirm("So your name is "+pc);
+//confirm("So your name is "+pc);
+
+Game();
+
+function Game(){
+    document.write("legend of zebra intake air from untamed land copyright");
+    var PlayerName = prompt("what is your name?");
+    alert("welcome to the land of hyreal copyright"+ PlayerName)
+    Prison();
+    function Prison(){
+        var Prison = prompt("you wake up in a prison but you cant remember why /n - look around /n- back to sleep /n - talk to man").toLowerCase;
+        
+        if(prison == "look around" || prison == "look"){
+            var prisonLook = prompt("its a small dirty prison there is a bared window to the back a man sleeps to the right to the front is a locked iron door to the left is your bed theres a rug in the center of the room /n -wake man /n -move rug /n eat bugs");
+        }
+        else if(prison == "go back to sleep" || prison == "sleep"){
+            alert("you sleep in your dirty bed like some pleb");
+            var resume = confirm("do you want to continue?");
+            
+            if(resume){
+                Prison();
+            }
+            else{
+                alert("Game Over");
+            }
+        }
+    }
+    
+}
